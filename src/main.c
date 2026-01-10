da52269bbe3764895b7cf794023b4bb734aab258
Kaden Wu <kadenwu07@gmail.com>
2026-01-10 13:46:00 -0700

    window

--- src/main.c
+++ src/main.c
@@ -1,4 +1,22 @@
-void test() {
-    // glipping it
-    // testing
+#include "raylib.h"
+
+int main() {
+
+    int width = 800;
+    int height = 600;
+    InitWindow(width, height, "gip");
+
+    while (!WindowShouldClose())
+    {
+
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            EndDrawing();
+    }
+
+    CloseWindow();
+    
+    return 0;
 }
